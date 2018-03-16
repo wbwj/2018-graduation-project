e26504c27c5c57cb1c3e3ca93166dee7b920e9af
dawang <854266726@qq.com>
2018-03-16 18:40:04 +0800

    face code

---
+++ code/qt+人脸识别/facerec-python-master/main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import sys
+import cv2
+
+from ui import mainwindow
+from camera import  Video
+from configure import config
+
+from PyQt4.QtGui import QApplication
+
+def main():
+    
+    model = cv2.createLBPHFaceRecognizer()
+    model.load(config.TRAINING_FILE)
+    
+    video = Video.Video(0)
+    video.setFrameSize(640, 480)
+    video.setFPS(30)
+    
+    QtApp = QApplication(sys.argv)
+    
+    mainWindow = mainwindow.Ui_MainWindow()   #界面
+    mainWindow.setModel(model)
+    mainWindow.showFullScreen()
+    mainWindow.setVideo(video)
+    mainWindow.raise_()
+    
+    QtApp.exec_()
+
+if __name__ == '__main__':
+    main()
